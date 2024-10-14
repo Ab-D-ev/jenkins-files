9a33934cc2fdfc6c554a34a3f13d4cee6e42d68e
Abdallah <abdallahq989@gmail.com>
2024-10-14 10:03:27 +0530

    Ace Pipeline fix 2

--- ace-windows-java.groovy
+++ ace-windows-java.groovy
@@ -30,9 +30,13 @@ pipeline {
             steps {
                 script {
                     // Using sshpass to SCP the file with the password
-                    sh '''
-                        sshpass -p "${SERVER_PASSWORD}" scp -o StrictHostKeyChecking=no /var/lib/jenkins/workspace/sap-vendor-java-test/target/ROOT.war ACEPortal1@98.70.57.5:"C:/Program\ Files/Apache\ Software\ Foundation/Tomcat\ 9.0/webapps/"
-                        '''
+                    // sh '''
+                    //     sshpass -p "${SERVER_PASSWORD}" scp -o StrictHostKeyChecking=no /var/lib/jenkins/workspace/sap-vendor-java-test/target/ROOT.war ACEPortal1@98.70.57.5:"C:/Program\ Files/Apache\ Software\ Foundation/Tomcat\ 9.0/webapps/"
+                    //     '''
+                    sh """
+                            sshpass -p '${SERVER_PASSWORD}' scp -o StrictHostKeyChecking=no /var/lib/jenkins/workspace/sap-vendor-java-test/target/ROOT.war ACEPortal1@98.70.57.5:\"C:/Program\\ Files/Apache\\ Software\\ Foundation/Tomcat\\ 9.0/webapps/\"
+                        """
+
                     
 
                 }
