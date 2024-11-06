99e481f31c212e724c1b4656346a262d39f145ac
Abdallah <abdallahq989@gmail.com>
2024-11-06 16:25:46 +0530

    Voltas pipeline added

---
+++ voltas-windows-react-test.groovy
@@ -0,0 +1,55 @@
+pipeline {
+    agent any
+
+    tools {
+        // Specify the Maven version configured in Global Tool Configuration
+        nodejs 'NodeJS 22' 
+    }
+     environment {
+        // SERVER_USER = 'ACEPortal1'
+        // SERVER_IP = '98.70.57.5'
+        SERVER_PASSWORD = 'Dextero@2022'  // Replace with the actual password
+        // FILE_TO_COPY = '/path/to/your/file.txt'  // Replace with the actual file path
+        // REMOTE_DEST = 'C:/Program Files/Apache Software Foundation/Tomcat 9.0/webapps' // Replace with the remote destination path
+    }
+    
+    stages {
+        stage('Checkout') {
+            steps {
+                git branch: 'main', credentialsId: 'Siba', url: 'git@github.com:sibananda485/ace-portal.git' 
+            }
+        }
+        
+        stage('Build') {
+            steps {
+                sh 'npm i'
+                sh 'npm run build'
+            }
+        }
+        
+        stage('Deploy') {
+            steps {
+                script {
+                    // Using sshpass to SCP the file with the password
+                    sh """
+                        sshpass -p '${SERVER_PASSWORD}' ssh -o StrictHostKeyChecking=no ACEPortal1@98.70.57.5 'powershell -Command "Remove-Item -Path \'C:/Users/ACEPortal1/Downloads/ACE/*\' -Recurse -Force"'
+                        """
+                    sh """
+                            sshpass -p '${SERVER_PASSWORD}' scp -o StrictHostKeyChecking=no -r /var/lib/jenkins/workspace/ace-windows-react-test/dist ACEPortal1@98.70.57.5:'"C:/Users/ACEPortal1/Downloads/ACE/"' 
+                        """
+
+
+                    
+
+                }
+                
+            }
+        }
+    }
+    
+    post {
+        always {
+            cleanWs()
+        }
+    }
+}
