c32d194f09497068172f5c1b74ecccfcf298bf4e
Abdallah <abdallahq989@gmail.com>
2024-11-11 11:45:56 +0530

    Ace Windows Test post deployment added

--- ace-windows-java.groovy
+++ ace-windows-java.groovy
@@ -49,9 +49,9 @@ pipeline {
    post {
         success {
             script {
-                emailext body: '''<html><body><h2>Ace Windows WAR Deployed - Version # $BUILD_NUMBER - Deployment $BUILD_STATUS.</h2><br/>
+                emailext body: '''<html><body><h2>Ace Windows Test WAR Deployed - Version # $BUILD_NUMBER - Deployment $BUILD_STATUS.</h2><br/>
                         <br/>
-                        <h2>Deployed on ace.actifyzone.com</h2>
+                        <h2>Deployed on ace-t.actifyzone.com</h2>
                         <br/>
                         <h3><b>Check console <a href="$BUILD_URL">output</a> to view full results.</b></h3><br/>
                         <b><i>If you cannot connect to the build server, check the attached logs.</i></b><br/>
@@ -63,14 +63,14 @@ pipeline {
                         <pre style='line-height: 22px; display: block; color: #333; font-family: Monaco,Menlo,Consolas,"Courier New",monospace; padding: 10.5px; margin: 0 0 11px; font-size: 13px; word-break: break-all; word-wrap: break-word; white-space: pre-wrap; background-color: #f5f5f5; border: 1px solid #ccc; border: 1px solid rgba(0,0,0,.15); -webkit-border-radius: 4px; -moz-border-radius: 4px; border-radius: 4px;'>
                         ${BUILD_LOG, maxLines=50, escapeHtml=true}
                         </pre></body></html>''',
-                    subject: 'Ace Windows WAR Deployed Successfully - $PROJECT_NAME',
+                    subject: 'Ace Windows Test WAR Deployed Successfully - $PROJECT_NAME',
                     to: 'omkar.more@dextero.in, abdallah.kammruddin@dextero.in',
                     mimeType: 'text/html'
             }
         }
         failure {
             script {
-                emailext body: '''<html><body><h2>Ace Windows WAR Failed - Build # $BUILD_NUMBER - Deployment $BUILD_STATUS.</h2><br/>
+                emailext body: '''<html><body><h2>Ace Windows Test WAR Failed - Build # $BUILD_NUMBER - Deployment $BUILD_STATUS.</h2><br/>
                         <br/>
                         <b>Check console <a href="$BUILD_URL">output</a> to view full results.</b><br/>
                         <b><i>If you cannot connect to the build server, check the attached logs.</i></b><br/>
@@ -82,7 +82,7 @@ pipeline {
                         <pre style='line-height: 22px; display: block; color: #333; font-family: Monaco,Menlo,Consolas,"Courier New",monospace; padding: 10.5px; margin: 0 0 11px; font-size: 13px; word-break: break-all; word-wrap: break-word; white-space: pre-wrap; background-color: #f5f5f5; border: 1px solid #ccc; border: 1px solid rgba(0,0,0,.15); -webkit-border-radius: 4px; -moz-border-radius: 4px; border-radius: 4px;'>
                         ${BUILD_LOG, maxLines=50, escapeHtml=true}
                         </pre><b>--LOG-END--</b></body></html>''',
-                    subject: 'Ace Windows WAR Deployment failed : $PROJECT_NAME',
+                    subject: 'Ace Windows Test WAR Deployment failed : $PROJECT_NAME',
                     to: 'abdallahq989@gmail.com, omkar.more@dextero.in',
                     mimeType: 'text/html'
             }
