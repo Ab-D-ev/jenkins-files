0d00e591ae84ab666b8f3d374a83ca5dc8fd32bd
Abdallah <abdallahq989@gmail.com>
2024-11-11 11:50:18 +0530

    Ace Windows PROD post deployment added

--- ace-windows-java-prod.groovy
+++ ace-windows-java-prod.groovy
@@ -46,9 +46,47 @@ pipeline {
         }
     }
     
-    post {
-        always {
-            cleanWs()
+     post {
+        success {
+            script {
+                emailext body: '''<html><body><h2>Ace Windows WAR Deployed - Version # $BUILD_NUMBER - Deployment $BUILD_STATUS.</h2><br/>
+                        <br/>
+                        <h2>Deployed on ace.actifyzone.com</h2>
+                        <br/>
+                        <h3><b>Check console <a href="$BUILD_URL">output</a> to view full results.</b></h3><br/>
+                        <b><i>If you cannot connect to the build server, check the attached logs.</i></b><br/>
+                        <br/>
+                        --<br/>
+                        Following is the last 50 lines of the log.<br/>
+                        <br/>
+                        <b>--LOG-BEGIN--</b><br/>
+                        <pre style='line-height: 22px; display: block; color: #333; font-family: Monaco,Menlo,Consolas,"Courier New",monospace; padding: 10.5px; margin: 0 0 11px; font-size: 13px; word-break: break-all; word-wrap: break-word; white-space: pre-wrap; background-color: #f5f5f5; border: 1px solid #ccc; border: 1px solid rgba(0,0,0,.15); -webkit-border-radius: 4px; -moz-border-radius: 4px; border-radius: 4px;'>
+                        ${BUILD_LOG, maxLines=50, escapeHtml=true}
+                        </pre></body></html>''',
+                    subject: 'Ace Windows WAR Deployed Successfully - $PROJECT_NAME',
+                    to: 'omkar.more@dextero.in, abdallah.kammruddin@dextero.in',
+                    mimeType: 'text/html'
+            }
+        }
+        failure {
+            script {
+                emailext body: '''<html><body><h2>Ace Windows WAR Failed - Build # $BUILD_NUMBER - Deployment $BUILD_STATUS.</h2><br/>
+                        <br/>
+                        <b>Check console <a href="$BUILD_URL">output</a> to view full results.</b><br/>
+                        <b><i>If you cannot connect to the build server, check the attached logs.</i></b><br/>
+                        <br/>
+                        --<br/>
+                        Following is the last 50 lines of the log.<br/>
+                        <br/>
+                        <b>--LOG-BEGIN--</b><br/>
+                        <pre style='line-height: 22px; display: block; color: #333; font-family: Monaco,Menlo,Consolas,"Courier New",monospace; padding: 10.5px; margin: 0 0 11px; font-size: 13px; word-break: break-all; word-wrap: break-word; white-space: pre-wrap; background-color: #f5f5f5; border: 1px solid #ccc; border: 1px solid rgba(0,0,0,.15); -webkit-border-radius: 4px; -moz-border-radius: 4px; border-radius: 4px;'>
+                        ${BUILD_LOG, maxLines=50, escapeHtml=true}
+                        </pre><b>--LOG-END--</b></body></html>''',
+                    subject: 'Ace Windows WAR Deployment failed : $PROJECT_NAME',
+                    to: 'abdallahq989@gmail.com, omkar.more@dextero.in',
+                    mimeType: 'text/html'
+            }
         }
     }
+
 }
